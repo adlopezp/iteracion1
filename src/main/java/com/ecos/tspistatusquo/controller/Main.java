f8b99228a4cffe3efd7da1f2e2497e8259db6dbb
Alvaro Lopez <ad.lopez11@uniandes.edu.co>
2015-04-14 01:17:54 -0500

    Cambiando Procfile

--- src/main/java/com/ecos/tspistatusquo/controller/Main.java
+++ src/main/java/com/ecos/tspistatusquo/controller/Main.java
@@ -43,7 +43,7 @@ public class Main extends HttpServlet {
      */
     public static void main(String[] args) throws Exception {
         Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-        //Server server = new Server(8080);
+//        Server server = new Server(5100);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
         server.setHandler(context);
