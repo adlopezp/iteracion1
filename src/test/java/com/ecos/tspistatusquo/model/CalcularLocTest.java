b3ac4868299db6b51b439a17059a2d49292bc8de
Alvaro Lopez <ad.lopez11@uniandes.edu.co>
2015-04-14 00:41:31 -0500

    Limpiando imports

--- src/test/java/com/ecos/tspistatusquo/model/CalcularLocTest.java
+++ src/test/java/com/ecos/tspistatusquo/model/CalcularLocTest.java
@@ -4,18 +4,12 @@
  */
 package com.ecos.tspistatusquo.model;
 
-import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.math.RoundingMode;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
 import junit.framework.TestCase;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
